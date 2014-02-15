d13ff733183038fbc7b8de786b0da1e3fbdf6e82
Sharat M R <cosmosgenius@gmail.com>
2014-02-15 22:20:02 +0530

    Why is the value not true

--- src/printarray.c
+++ src/printarray.c
@@ -9,7 +9,16 @@ int main()
 
     printf("Size of array %d\n", TOTAL_ELEMENTS);
     d = -1;
-    printf("value of %d\n", d <= (TOTAL_ELEMENTS-2));
+    
+    printf("value of d is %d\n", d);
+    printf("value of TOTAL_ELEMENTS is %d\n", TOTAL_ELEMENTS);
+
+    if(d <= TOTAL_ELEMENTS){
+        printf("true d is less than/equal TOTAL_ELEMENTS\n");    
+    }else{
+        printf("false d is greater than TOTAL_ELEMENTS\n");
+    }
+
     for(d=-1;d <= (TOTAL_ELEMENTS-2);d++){
         printf("value of %d\n",d);
         printf("%d\n",array[d+1]);
